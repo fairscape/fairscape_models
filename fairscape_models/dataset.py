b469cf4ef756c53891428a5d08c0f05c28d3e0d4
Justin <jniestroy@gmail.com>
2025-03-25 15:19:59 -0400

    missing import

--- fairscape_models/dataset.py
+++ fairscape_models/dataset.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional, List, Union
 
 from fairscape_models.fairscape_base import IdentifierValue, DATASET_TYPE
