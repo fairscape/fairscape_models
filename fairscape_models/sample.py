f11e43f812e8363cea9e97e48befb69a2af7ef6d
jniestroy <jniestroy@gmail.com>
2025-11-04 14:00:33 -0500

    allow short sample names

--- fairscape_models/sample.py
+++ fairscape_models/sample.py
@@ -7,7 +7,7 @@ class Sample(BaseModel):
     name: str
     metadataType: Optional[str] = Field(default="https://w3id.org/EVI#Sample", alias="@type")
     author: Union[str, List[str]]
-    description: str = Field(min_length=10)
+    description: str = Field(min_length=1)
     keywords: List[str] = Field(...)
     contentUrl: Optional[Union[str, List[str]]] = Field(default=None)
     cellLineReference: Optional[IdentifierValue] = Field(default=None) 
