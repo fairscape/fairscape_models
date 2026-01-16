24272d71a54246efd8adc6e855c23d13aca216db
jniestroy <jniestroy@gmail.com>
2026-01-16 10:10:20 -0500

    export context and naan

--- fairscape_models/__init__.py
+++ fairscape_models/__init__.py
@@ -6,7 +6,7 @@ from fairscape_models.computation import Computation
 from fairscape_models.dataset import Dataset
 from fairscape_models.software import Software
 from fairscape_models.mlmodel import MLModel
-from fairscape_models.fairscape_base import IdentifierValue, IdentifierPropertyValue
+from fairscape_models.fairscape_base import IdentifierValue, IdentifierPropertyValue, DEFAULT_ARK_NAAN, DEFAULT_LICENSE, DEFAULT_CONTEXT
 from fairscape_models.medical_condition import MedicalCondition
 from fairscape_models.schema import Schema
 from fairscape_models.rocrate import ROCrateV1_2, ROCrateMetadataElem, ROCrateMetadataFileElem, ROCrateDistribution, GenericMetadataElem
